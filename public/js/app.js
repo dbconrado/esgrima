28fc70547c8ddaaa4859f8f7f898ff9eacf2e58d
dbconrado <dbconrado@gmail.com>
2018-07-01 22:41:12 -0300

    refactoring: separando pedaços de código em unidades mais coesas

--- public/js/app.js
+++ public/js/app.js
@@ -10,14 +10,14 @@
     document.querySelectorAll('.atleta')
       .forEach(e => gerenciadorToques.posicionarMarcadores(e));
 
-    // FIXME: deixar apontar o local da pista apenas uma vez
-    var pista = document.querySelector('#pista');
-    var hammer = new Hammer(pista);
-
-    hammer.on('tap', (ev) => {
-    	pista.appendChild(gerenciadorToques.novoToque(ev.center.x, ev.center.y));
-    });
-
-
   });
+
+  // FIXME: deixar apontar o local da pista apenas uma vez
+  var pista = document.querySelector('#pista');
+  var hammer = new Hammer(pista);
+
+  hammer.on('tap', (ev) => {
+    pista.appendChild(gerenciadorToques.novoToque(ev.center.x, ev.center.y));
+  });
+
 })();
