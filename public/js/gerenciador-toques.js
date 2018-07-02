9b9f3e6cde68a62c44526ef19855615a29ec2377
dbconrado <dbconrado@gmail.com>
2018-07-02 17:57:26 -0300

    refactoring

--- public/js/gerenciador-toques.js
+++ public/js/gerenciador-toques.js
@@ -142,3 +142,30 @@ var GerenciadorToques = function() {
 		configurarMarcadores: configurarMarcadores
 	};
 };
+
+var MarcadorTracker = function() {
+	this.marcadores = {
+		cabeca: {selecionado: false},
+		tronco: {selecionado: false},
+		bracoEsquerdo: {selecionado: false},
+		bracoDireito: {selecionado: false},
+		maoEsquerda: {selecionado: false},
+		maoDireita: {selecionado: false},
+		pernaEsquerda: {selecionado: false},
+		pernaDireita: {selecionado: false},
+		peEsquerdo: {selecionado: false},
+		peDireito: {selecionado: false},
+	};
+
+	this.toggle = function(marcador) {
+		var valorOriginal = this.marcadores[marcador].selecionado;
+		this.marcadores[marcador].selecionado = !valorOriginal;
+		Object.entries(this.marcadores)
+			.filter(entry => entry[0] !== marcador)
+			.forEach(entry => entry[1].selecionado = false);
+	}
+
+	this.isSelecionado = function(marcador) {
+		return this.marcadores[marcador].selecionado;
+	}
+}
